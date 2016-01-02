2523f8b67ea5a70292c50217b057f3967a39c1a2
aparna krishnan <aparnalocked@gmail.com>
2016-01-02 14:54:16 +0400

    functional

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -7,19 +7,79 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    var tField: UITextField!
+    var items: [Item] = []
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func configTfield(textField:UITextField){
+        textField.placeholder = "add new item"
+        self.tField = textField
+        
     }
-
+    func alertpopup() {
+        
+        let alert = UIAlertController(title: "Add New Item", message: nil, preferredStyle: .Alert)
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel){
+            UIAlertAction in
+            alert.dismissViewControllerAnimated(true, completion: nil)
+            
+        }
+        let save = UIAlertAction(title: "Save", style: UIAlertActionStyle.Default){
+            UIAlertAction in
+            self.saveaction()
+        }
+        alert.addTextFieldWithConfigurationHandler(configTfield)
+        alert.addAction(cancel)
+        alert.addAction(save)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    func saveaction(){
+        print("SAVED")
+        let context = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
+        let item = NSEntityDescription.insertNewObjectForEntityForName("Item", inManagedObjectContext: context) as! Item
+        item.title = tField.text
+        
+        do {
+            try context.save()
+        }
+        catch _{
+            
+        }
+        let request = NSFetchRequest(entityName: "Item")
+        let results: [AnyObject]?
+        do{
+            try results = context.executeFetchRequest(request)
+        }
+        catch _{
+            results = nil
+        }
+        if results != nil{
+            self.items = results as! [Item]
+        }
+        self.tableView.reloadData()
+        
+        
+    }
+    @IBAction func addButtonPressed(sender: AnyObject) {
+        alertpopup()
+            }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.items[indexPath.row].title
+        return cell
+    }
 
 }
 
