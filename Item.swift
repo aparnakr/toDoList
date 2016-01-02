5b793e35802f07e76976992457f7925d24bae087
aparna krishnan <aparnalocked@gmail.com>
2016-01-02 14:48:08 +0400

    first version

---
+++ Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Aparna Krishnan on 12/31/15.
+//  Copyright © 2015 Aparna. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Item: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
